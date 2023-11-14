e25537de44542553b41df679ba6a0e4295ffd7f4
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 <toluwalopeogunniyi@gmail.com>
2023-11-14 15:19:49 -0800

    made changes to header file

--- shell.h
+++ shell.h
@@ -1,1 +1,15 @@
-#
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/type.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <limits.h>
+
+
